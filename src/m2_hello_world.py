ad5132a96e50febe69b4e94e76f7a01b20e007dc
tsaijc <tsaijc@rose-hulman.edu>
2017-11-27 10:28:52 -0500

    completed todos

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -13,7 +13,7 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
 #   Run this module by right clicking anywhere in this window and select
@@ -26,7 +26,7 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 2.
+# DONE:2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a TO DO in this module.
 #
@@ -34,7 +34,7 @@ print('Hello, World')
 #        by clicking on the blue bars.  ** Try that now. **
 #
 #     b. When you have completed a TO DO, you should change the word
-#          TODO
+#          DONE
 #        to
 #          DONE.
 #        Try it now on line 16 above, and note that its blue bar on
@@ -51,8 +51,9 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 3.
-#   Add another   print   statement below.
+# DONE: 3.
+#   Add another   print   statement below.#
+print('hello')
 #   It should print any string that you want (but keep it G-rated!)
 #   Test your code by re-running this module using either the right click
 #       method again or by using the play button in the upper right.
@@ -62,11 +63,12 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
-#   You do NOT have to use strings for this, so no quotation marks!
+#   You do NOT have to use strings for this, so no quotation marks!#
+print(3607*34227)
 #
 #   TEST your code by re-running this module, then asking someone
 #   whom you trust:  What number did your   print   display for TO DO 4?
